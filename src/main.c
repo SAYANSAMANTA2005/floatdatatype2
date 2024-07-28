55b3aa7f9283110d68a8f0818f1336b769fce36b
root <root@localhost>
2024-07-28 21:26:07 +0530

    auto publish 1722182167107

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+   int x=1;
+   int y=2;
+   int r=x*x+y*y; 
+   printf("input x=1 and y=2");
+   
+   printf("\n square of moduls [r]of coordinate x,y is:%d"r);
+ 
+   printf("\n the value of r^2+r is:%d",r*r +r);
+ 
+   
+   
+   
+   
+   
+   
+}
